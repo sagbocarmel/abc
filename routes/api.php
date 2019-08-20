4beb1fd55a0a750b99cd65583ed16031f54c33ce
BaHithBENON <52800154+BaHithBENON@users.noreply.github.com>
2019-08-20 13:34:01 +0200

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -26,5 +26,25 @@ Route::middleware('auth:api')->group( function () {
 
 
 
+//Routes vers les notes
+Route::post('note', 'NotesController@insertNote');
+Route::post('note/update', 'NotesController@updateNote');
+Route::post('note/delete', 'NotesController@deleteNote');
+Route::post('note/search', 'NotesController@searchNote');
+Route::post('note/index', 'NotesController@getListNote');
 
+//Routes vers les moyennes
+Route::post('moyenne/matiere', 'MoyennesController@updateMoyenneMatiere');
+Route::post('moyenne/periode', 'MoyennesController@updateMoyennePeriode');
+Route::post('moyenne/annuelle', 'MoyennesController@updateMoyenneAnnuelle');
+
+//Routes vers les evaluations
+Route::post('evaluation/add', 'EvaluationsController@add');
+Route::post('evaluation/update', 'EvaluationsController@update');
+Route::get('evaluation/search', 'EvaluationsController@search');
+Route::post('evaluation/delete', 'EvaluationsController@delete');
+Route::get('evaluation/index', 'EvaluationsController@getList');
+Route::get('evaluation/details', 'EvaluationsController@getDetails');
+Route::get('evaluation/count', 'EvaluationsController@count');
+Route::post('evaluation/attr', 'EvaluationsController@attribuerNotes');
 
